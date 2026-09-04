47307f8d5565ff6cc18a4cdb027c510c1dce0a38
Sagar02k4 <dev.sagar2k4@gmail.com>
2026-09-05 00:28:14 +0530

    Added Task Entity, Repository, Services and Controller with basic CRUD APIs

---
+++ src/main/java/com/sagar/taskscheduler/controller/TaskController.java
@@ -0,0 +1,36 @@
+package com.sagar.taskscheduler.controller;
+
+import com.sagar.taskscheduler.model.Task;
+import com.sagar.taskscheduler.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
+}
